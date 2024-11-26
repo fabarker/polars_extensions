cde8d4d113ae0a76d4a1c6949fe63e0093ca3d17
Epsilon Psi <123123634+fabarker@users.noreply.github.com>
2024-11-26 16:38:53 +0000

    add new rolling window trait to handle exponentially decaying weights for rolling stats e.g. ew rolling mean, var

--- crates/polars-windowing/src/rolling/mod.rs
+++ crates/polars-windowing/src/rolling/mod.rs
@@ -1,7 +1,7 @@
 use std::fmt::Debug;
 use std::iter;
 use std::iter::Sum;
-use std::ops::{AddAssign, Div, DivAssign, MulAssign, SubAssign};
+use std::ops::{AddAssign, Div, DivAssign, Mul, MulAssign, SubAssign};
 
 use num_traits::{Float, Num, NumCast};
 use polars::datatypes::{Float32Type, Float64Type};
@@ -13,11 +13,11 @@ use polars_core::datatypes::DataType::{Float32, Float64};
 use polars_core::datatypes::PolarsNumericType;
 use polars_core::prelude::ChunkedArray;
 use polars_core::series::Series;
-use polars_custom_utils::utils::weights::coerce_weights;
+use polars_custom_utils::utils::weights::{coerce_weights, ExponentialDecayType, ExponentialWeights};
 use polars_utils::float::IsFloat;
 use polars_utils::index::NullCount;
-
-use crate::MyArrayExt;
+use rand_distr::Exp;
+use crate::{ExponentiallyWeighted, MyArrayExt};
 
 pub mod cagr;
 pub mod kurtosis;
@@ -50,15 +50,30 @@ pub(super) struct SumSquaredWindow<'a, T> {
     last_start: usize,
     last_end: usize,
     validity: Option<&'a Bitmap>,
-    // if we don't recompute every 'n' iterations
-    // we get a accumulated error/drift
     last_recompute: u8,
     pub(super) null_count: usize,
+}
+
+pub(super) struct ExponentialSumSquaredWindow<'a, T> {
+    slice: &'a [T],
+    sum_of_squares: Option<T>,
+    last_start: usize,
+    last_end: usize,
+    validity: Option<&'a Bitmap>,
+    last_recompute: u8,
+    pub(super) null_count: usize,
+    weights: &'a ExponentialWeights<T>
 }
 
 pub struct VarWindow<'a, T> {
     mean: MeanWindow<'a, T>,
     sum_of_squares: SumSquaredWindow<'a, T>,
+    ddof: u8,
+}
+
+pub struct ExponentialVarWindow<'a, T> {
+    sum: ExponentialSumWindow<'a, T>,
+    sum_of_squares: ExponentialSumSquaredWindow<'a, T>,
     ddof: u8,
 }
 
@@ -71,8 +86,22 @@ pub struct SumWindow<'a, T> {
     pub(super) null_count: usize,
 }
 
+pub struct ExponentialSumWindow<'a, T> {
+    slice: &'a [T],
+    validity: Option<&'a Bitmap>,
+    sum: Option<T>,
+    last_start: usize,
+    last_end: usize,
+    pub(super) null_count: usize,
+    weights: &'a ExponentialWeights<T>,
+}
+
 pub struct MeanWindow<'a, T> {
     sum: SumWindow<'a, T>,
+}
+
+pub struct ExponentialMeanWindow<'a, T> {
+    sum: ExponentialSumWindow<'a, T>,
 }
 
 pub struct ProdWindow<'a, T> {
@@ -84,17 +113,31 @@ pub struct ProdWindow<'a, T> {
     pub(super) null_count: usize,
 }
 
+pub struct ExponentialProdWindow<'a, T> {
+    slice: &'a [T],
+    validity: Option<&'a Bitmap>,
+    prod: Option<T>,
+    last_start: usize,
+    last_end: usize,
+    pub(super) null_count: usize,
+    weights: &'a ExponentialWeights<T>
+}
+
 pub trait RollingAggWindow<'a, T: NativeType> {
-    unsafe fn update(&mut self, start: usize, end: usize) -> Option<T>;
-
-    unsafe fn update_nulls(&mut self, start: usize, end: usize) -> Option<T>;
-
-    unsafe fn update_no_nulls(&mut self, start: usize, end: usize) -> Option<T>;
-
     unsafe fn new(slice: &'a [T], validity: Option<&'a Bitmap>, start: usize, end: usize) -> Self;
-
+    unsafe fn update(&mut self, start: usize, end: usize) -> Option<T>;
+    unsafe fn update_nulls(&mut self, start: usize, end: usize) -> Option<T>;
+    unsafe fn update_no_nulls(&mut self, start: usize, end: usize) -> Option<T>;
     fn is_valid(&self, min_periods: usize) -> bool;
+    fn window_type() -> &'static str;
+}
 
+pub trait EWRollingAggWindow<'a, T: NativeType> {
+    unsafe fn new(slice: &'a [T], validity: Option<&'a Bitmap>, start: usize, end: usize, decay_type: &'a ExponentialWeights<T>) -> Self;
+    unsafe fn update(&mut self, start: usize, end: usize) -> Option<T>;
+    unsafe fn update_nulls(&mut self, start: usize, end: usize) -> Option<T>;
+    unsafe fn update_no_nulls(&mut self, start: usize, end: usize) -> Option<T>;
+    fn is_valid(&self, min_periods: usize) -> bool;
     fn window_type() -> &'static str;
 }
 
@@ -150,52 +193,42 @@ where
     }
 }
 
-/*
-fn rolling_aggregator<'a, Agg, T, U>(
-    ca: &'a ChunkedArray<U>,
+
+fn apply_ew_rolling_aggregator_chunked<T>(
+    ca: &ChunkedArray<T>,
     window_size: usize,
     min_periods: usize,
     center: bool,
-    weights: Option<Vec<f64>>,
+    decay: &ExponentialDecayType,
+    aggregator_fn: &dyn Fn(
+        &PrimitiveArray<T::Native>,
+        usize,
+        usize,
+        bool,
+        ExponentialWeights<T::Native>,
+    ) -> ArrayRef,
 ) -> PolarsResult<Series>
 where
-    Agg: RollingAggWindow<'a, T>,
-    U: PolarsNumericType<Native = T>,
-    T: NativeType
-    + iter::Sum
-    + iter::Product
+    T: PolarsNumericType,
+    T::Native: Float
+    + Copy
+    + Mul<Output = T::Native>
+    + PartialEq
     + NumCast
-    + AddAssign
-    + SubAssign
+    + num_traits::Zero
+    + Sum
+    + std::ops::Add<Output = T::Native>
+    + Div<Output = T::Native>
     + DivAssign
-    + MulAssign
-    + Num
-    + PartialOrd,
+    + std::ops::Sub<Output = T::Native>,
 {
-    //let ca = ca.rechunk();
+
+    let wts = ExponentialWeights::<T::Native>::new(decay.clone(), window_size as i32, false);
+    let ca = ca.rechunk();
     let arr = ca.downcast_iter().next().unwrap();
-    let arr = match ca.null_count() {
-        0 => rolling_aggregator_no_nulls::<Agg, _>(
-            arr.values().as_slice(),
-            arr.validity(),
-            window_size,
-            min_periods,
-            center,
-            weights.as_deref(),
-        )?,
-        _ => rolling_aggregator_nulls::<Agg, _>(
-            arr.values().as_slice(),
-            arr.validity(),
-            window_size,
-            min_periods,
-            center,
-            weights.as_deref(),
-        )?,
-    };
+    let arr = aggregator_fn(&arr, window_size, min_periods, center, wts);
     Series::try_from((ca.name().clone(), arr))
 }
-
- */
 
 fn apply_rolling_aggregator_chunked<T>(
     ca: &ChunkedArray<T>,
@@ -223,6 +256,7 @@ where
 
 trait WindowType<'a, T: NativeType> {
     type Window: 'a + RollingAggWindow<'a, T>;
+    type EWindow: 'a + EWRollingAggWindow<'a, T>;
     fn get_weight_computer() -> fn(&[T], &[T]) -> T;
 
     fn prepare_weights(weights: Vec<T>) -> Vec<T>;
@@ -293,5 +327,43 @@ where
                 &W::prepare_weights(weights),
             )
         },
+    }
+}
+
+
+fn calc_ew_rolling_generic<'a, T, W>(
+    arr: &'a PrimitiveArray<T>,
+    window_size: usize,
+    min_periods: usize,
+    center: bool,
+    decay: &'a ExponentialWeights<T>,
+) -> ArrayRef
+where
+    T: NativeType + Float + iter::Sum<T> + SubAssign + AddAssign + IsFloat,
+    W: WindowType<'a, T>,
+{
+    let offsets: OffsetFn = if center {
+        det_offsets_center
+    } else {
+        det_offsets
+    };
+
+    match arr.has_nulls() {
+        true => nulls::calc_ew_rolling_aggregator::<W::EWindow, T, OffsetFn>(
+            arr.values().as_slice(),
+            arr.validity(),
+            window_size,
+            min_periods,
+            offsets,
+            &decay,
+        ),
+        false => no_nulls::calc_ew_rolling_aggregator::<W::EWindow, T, OffsetFn>(
+            arr.values().as_slice(),
+            arr.validity(),
+            window_size,
+            min_periods,
+            offsets,
+            &decay,
+        ),
     }
 }
